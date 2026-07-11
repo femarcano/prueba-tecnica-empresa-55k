48113878400aade2202d25954b7218b533db8bd8
Fernando Marcano <26585950+femarcano@users.noreply.github.com>
2026-07-11 09:37:54 +0900

    chore: Add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': 'src',
+      '@assets': 'src/assets',
+      '@/utils': 'src/utils',
+      '@/features': 'src/features',
+      '@/repositories': 'src/repositories',
+      '@/types': 'src/types',
+    }
+  }
 })
